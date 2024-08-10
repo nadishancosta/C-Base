d92cb38f6f67cc12354dee4bcfb6c93e0b7d8f00
nadishancosta <nadishancosta@gmail.com>
2024-08-10 23:30:33 +0100

    cleaning repo

--- Learning C/bitwise.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main (void)
-{
-	fuckckckckckckc.xld.didmd.sjfdw.siknflerjoeffiejl,d,efmiekrk vljfkwejk'weleke';fe;;p'rfoeroowejg.wnfieihketi
-gkwetwjgk4ktgw.,vweioevwoivjwe0odf.vewfvp-ekflvpefjvwi
-\vewnvoelke,kvowejpenolkgviow3
-}
--- Learning C/first.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-main()
-{
-	printf("\n\n\n\n\nMelathi I love YOU :D XD!!!!!!!!!!! \n\n\n\n\n");
-	return 0;
-}
